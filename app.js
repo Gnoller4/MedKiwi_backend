fb7ad320688b262c5993ec24ca6cf151a9657aac
philippnirnberger <nirnberger@akaryon.com>
2021-01-29 21:15:24 +0100

    changed deployment; rmoved robots.txt and humans.txt

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -17,6 +18,6 @@ io.on('connection', (socket) => {
   });
 });
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+http.listen(port, () => {
+  console.log('listening on *:'+port);
 });
